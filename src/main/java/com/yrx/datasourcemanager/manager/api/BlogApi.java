56512249bc2b7670cdbd008e0cf711904f0836ad
xin <18825188512@163.com>
2019-10-02 01:02:26 +0800

    feat(): post请求报文

---
+++ src/main/java/com/yrx/datasourcemanager/manager/api/BlogApi.java
@@ -0,0 +1,65 @@
+package com.yrx.datasourcemanager.manager.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by r.x on 2019/10/1.
+ */
+@RestController
+@RequestMapping("/blog")
+@Slf4j
+public class BlogApi {
+
+    @PostMapping("/api")
+    public String blog() {
+        log.info("blog api");
+        String source = "# spring boot整合mybatis \n" +
+                "spring boot项目整合mybatis \n" +
+                "### jdbc配置 \n" +
+                "``` \n" +
+                "spring.datasource.url=jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true \n" +
+                "spring.datasource.username=root \n" +
+                "spring.datasource.password=root \n" +
+                "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver \n" +
+                "```\n" +
+                "### mybatis相关配置 \n" +
+                "``` xml\n" +
+                "<?xml version=\\1.0\\ encoding=\\UTF-8\\?> \n" +
+                "<!DOCTYPE generatorConfiguration \n" +
+                "  PUBLIC \\-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN\\ \n" +
+                "  \\http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd\\> \n" +
+                " \n" +
+                "<generatorConfiguration> \n" +
+                "    <classPathEntry location=\\C:\\\\Users\\\\r.x\\\\.m2\\\\repository\\\\mysql\\\\mysql-connector-java\\\\8.0.17\\\\mysql-connector-java-8.0.17.jar\\ /> \n" +
+                " \n" +
+                "    <context id=\\DB2Tables\\ targetRuntime=\\MyBatis3\\> \n" +
+                "        <jdbcConnection driverClass=\\com.mysql.cj.jdbc.Driver\\ \n" +
+                "                        connectionURL=\\jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC\\ \n" +
+                "                        userId=\\root\\ \n" +
+                "                        password=\\root\\> \n" +
+                "```\n" +
+                "### mapper代码示例 \n" +
+                "``` java\n" +
+                " \n" +
+                "package com.yrx.datasourcemanager.manager.dao.extend; \n" +
+                " \n" +
+                "import com.yrx.datasourcemanager.manager.pojo.ParamConfig; \n" +
+                "import org.apache.ibatis.annotations.Result; \n" +
+                "import org.apache.ibatis.annotations.Results; \n" +
+                "import org.apache.ibatis.annotations.Select; \n" +
+                "import org.apache.ibatis.type.JdbcType; \n" +
+                " \n" +
+                "import java.util.List; \n" +
+                " \n" +
+                "/** \n" +
+                " * Created by r.x on 2019/9/2. \n" +
+                " */ \n" +
+                "public interface ParamConfigExtendMapper { \n" +
+                "} \n" +
+                "```";
+        return source;
+    }
+}
--- src/main/resources/static/model/home.js
+++ src/main/resources/static/model/home.js
@@ -11,51 +11,115 @@ function showTime() {
     setTimeout("showTime()", 1000); //设置过1000毫秒就是1秒，调用show方法
 }
 
-function parseBlog() {
-    var blog = "# spring boot整合mybatis\n" +
-        "spring boot项目整合mybatis\n" +
-        "### jdbc配置\n" +
-        "``` \n" +
-        "spring.datasource.url=jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true\n" +
-        "spring.datasource.username=root\n" +
-        "spring.datasource.password=root\n" +
-        "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver\n" +
-        "```\n" +
-        "### mybatis相关配置\n" +
-        "``` xml\n" +
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-        "<!DOCTYPE generatorConfiguration\n" +
-        "  PUBLIC \"-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN\"\n" +
-        "  \"http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd\">\n" +
-        "\n" +
-        "<generatorConfiguration>\n" +
-        "    <classPathEntry location=\"C:\\Users\\r.x\\.m2\\repository\\mysql\\mysql-connector-java\\8.0.17\\mysql-connector-java-8.0.17.jar\" />\n" +
-        "\n" +
-        "    <context id=\"DB2Tables\" targetRuntime=\"MyBatis3\">\n" +
-        "        <jdbcConnection driverClass=\"com.mysql.cj.jdbc.Driver\"\n" +
-        "                        connectionURL=\"jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC\"\n" +
-        "                        userId=\"root\"\n" +
-        "                        password=\"root\">\n" +
-        "```\n" +
-        "### mapper代码示例\n" +
-        "``` java\n" +
-        "\n" +
-        "package com.yrx.datasourcemanager.manager.dao.extend;\n" +
-        "\n" +
-        "import com.yrx.datasourcemanager.manager.pojo.ParamConfig;\n" +
-        "import org.apache.ibatis.annotations.Result;\n" +
-        "import org.apache.ibatis.annotations.Results;\n" +
-        "import org.apache.ibatis.annotations.Select;\n" +
-        "import org.apache.ibatis.type.JdbcType;\n" +
-        "\n" +
-        "import java.util.List;\n" +
-        "\n" +
-        "/**\n" +
-        " * Created by r.x on 2019/9/2.\n" +
-        " */\n" +
-        "public interface ParamConfigExtendMapper {\n" +
-        "}\n" +
-        "```";
+function loadBlog() {
+    var url = "http://localhost:9090/blog/api"
+    ajaxPost(url, null, parseBlog, null, null)
+    var xmlHttp;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp = new XMLHttpRequest();
+    } catch (e) {
+        // Internet Explorer
+        try {
+            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("您的浏览器不支持AJAX！");
+                return false;
+            }
+        }
+    }
+    return xmlHttp;
+}
+
+// ajax post请求：
+function ajaxPost(url, data, fnSucceed, fnFail, fnLoading) {
+    var ajax = ajaxObject();
+    ajax.open("post", url, true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState === 4) {
+            if (ajax.status === 200) {
+                fnSucceed(ajax.responseText);
+            } else {
+                fnFail("HTTP请求错误！错误码：" + ajax.status);
+            }
+        } else {
+            fnLoading();
+        }
+    };
+    ajax.send(data);
+}
+
+// ajax 对象
+function ajaxObject() {
+    var xmlHttp;
+    try {
+        // Firefox, Opera 8.0+, Safari
+        xmlHttp = new XMLHttpRequest();
+    } catch (e) {
+        // Internet Explorer
+        try {
+            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                alert("您的浏览器不支持AJAX！");
+                return false;
+            }
+        }
+    }
+    return xmlHttp;
+}
+
+function parseBlog(blog) {
+    // var blog = "# spring boot整合mybatis\n" +
+    //     "spring boot项目整合mybatis\n" +
+    //     "### jdbc配置\n" +
+    //     "``` \n" +
+    //     "spring.datasource.url=jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC&useUnicode=true&characterEncoding=utf-8&useSSL=true\n" +
+    //     "spring.datasource.username=root\n" +
+    //     "spring.datasource.password=root\n" +
+    //     "spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver\n" +
+    //     "```\n" +
+    //     "### mybatis相关配置\n" +
+    //     "``` xml\n" +
+    //     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+    //     "<!DOCTYPE generatorConfiguration\n" +
+    //     "  PUBLIC \"-//mybatis.org//DTD MyBatis Generator Configuration 1.0//EN\"\n" +
+    //     "  \"http://mybatis.org/dtd/mybatis-generator-config_1_0.dtd\">\n" +
+    //     "\n" +
+    //     "<generatorConfiguration>\n" +
+    //     "    <classPathEntry location=\"C:\\Users\\r.x\\.m2\\repository\\mysql\\mysql-connector-java\\8.0.17\\mysql-connector-java-8.0.17.jar\" />\n" +
+    //     "\n" +
+    //     "    <context id=\"DB2Tables\" targetRuntime=\"MyBatis3\">\n" +
+    //     "        <jdbcConnection driverClass=\"com.mysql.cj.jdbc.Driver\"\n" +
+    //     "                        connectionURL=\"jdbc:mysql://localhost:3306/data_source?serverTimezone=UTC\"\n" +
+    //     "                        userId=\"root\"\n" +
+    //     "                        password=\"root\">\n" +
+    //     "```\n" +
+    //     "### mapper代码示例\n" +
+    //     "``` java\n" +
+    //     "\n" +
+    //     "package com.yrx.datasourcemanager.manager.dao.extend;\n" +
+    //     "\n" +
+    //     "import com.yrx.datasourcemanager.manager.pojo.ParamConfig;\n" +
+    //     "import org.apache.ibatis.annotations.Result;\n" +
+    //     "import org.apache.ibatis.annotations.Results;\n" +
+    //     "import org.apache.ibatis.annotations.Select;\n" +
+    //     "import org.apache.ibatis.type.JdbcType;\n" +
+    //     "\n" +
+    //     "import java.util.List;\n" +
+    //     "\n" +
+    //     "/**\n" +
+    //     " * Created by r.x on 2019/9/2.\n" +
+    //     " */\n" +
+    //     "public interface ParamConfigExtendMapper {\n" +
+    //     "}\n" +
+    //     "```";
     var lines = blog.split("\n");
     var code = "";
     var language = "";
