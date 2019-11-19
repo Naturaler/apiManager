19b9516f68eb499c61d920c009b9ac424c7290a5
xin <18825188512@163.com>
2019-11-20 01:38:05 +0800

    feat(): 逻辑混乱了

--- src/main/java/com/yrx/datasourcemanager/manager/util/PlantUmlUtil.java
+++ src/main/java/com/yrx/datasourcemanager/manager/util/PlantUmlUtil.java
@@ -2,12 +2,19 @@ package com.yrx.datasourcemanager.manager.util;
 
 import com.alibaba.fastjson.JSON;
 import lombok.Data;
+import net.sourceforge.plantuml.SourceStringReader;
+import org.springframework.util.StringUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -17,18 +24,76 @@ public class PlantUmlUtil {
 
     public static void main(String[] args) {
         System.out.println("TraceLog.convertStrToBean(\"\") = " + TraceLog.convertStrToBean(""));
+        readAndGenPng("E:\\tmp\\agent\\trace_v2.txt");
     }
 
     public static boolean readAndGenPng(String log) {
+        Stack<TraceLog> stack = new Stack<>();
+        StringBuilder builder = new StringBuilder();
+        String lineSeparator = System.lineSeparator();
+        builder.append("@startuml").append(lineSeparator);
+        builder.append("autonumber").append(lineSeparator);
+        builder.append(lineSeparator);
         try {
             Path path = new File(log).toPath();
             Stream<String> lines = Files.lines(path);
+            List<TraceLog> list = lines
+                    .filter(line -> !StringUtils.isEmpty(line))
+                    .map(TraceLog::convertStrToBean)
+                    .collect(Collectors.toList());
+            for (TraceLog traceLog : list) {
+                if (traceLog.flag.equals("before")) {
+                    if (stack.size() > 0) {
+                        paint(stack.peek(), traceLog, builder, " -> ", lineSeparator);
+                    }
+                    stack.push(traceLog);
+                } else {
+                    TraceLog peek = stack.peek();
+                    if (peek.className.equals(traceLog.className) && peek.methodName.equals(traceLog.methodName) && peek.flag.equals("before")) {
+                        paint(stack.peek(), traceLog, builder, " <- ", lineSeparator);
+                    }
+                    paint(traceLog, stack.pop(), builder, " <- ", lineSeparator);
 
+                }
+            }
+            builder.append("@enduml").append(lineSeparator);
+            System.out.println("builder.toString() = " + builder.toString());
+            genPng(builder);
             return true;
         } catch (IOException e) {
             e.printStackTrace();
             return false;
         }
+    }
+
+    private static String getDirectionByFlag(String flag) {
+        switch (flag) {
+            case "before":
+                return " -> ";
+            case "after":
+                return " <- ";
+            default:
+                return " -> ";
+        }
+    }
+
+    private static void genPng(StringBuilder builder) {
+        try {
+            OutputStream png = new FileOutputStream(new File("E:\\tmp\\agent\\seq.png"));
+            SourceStringReader reader = new SourceStringReader(builder.toString());
+            String image = reader.generateImage(png);
+            System.out.println(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void paint(TraceLog first, TraceLog second, StringBuilder builder, String direction, String lineSeparator) {
+        builder.append(first.className.replace("/", "_")).append("_").append(first.methodName)
+                .append(direction)
+                .append(second.className.replace("/", "_")).append("_").append(second.methodName)
+                .append(": ")
+                .append(lineSeparator);
     }
 
 
@@ -41,8 +106,7 @@ public class PlantUmlUtil {
         private String methodName;
 
         static TraceLog convertStrToBean(String source) {
-            String s = "{\"className\":\"com/yrx/datasourcemanager/manager/pojo/BlogExample\",\"flag\":\"after\",\"methodName\":\"createCriteria\",\"thread\":\"http-nio-9090-exec-10\",\"time\":1574094544236}";
-            return JSON.parseObject(s, TraceLog.class);
+            return JSON.parseObject(source, TraceLog.class);
         }
     }
 }
