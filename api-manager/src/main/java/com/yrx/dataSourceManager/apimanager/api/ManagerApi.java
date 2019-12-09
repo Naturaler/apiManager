f26e165bcfac668f9ba67c40d5d4db6642d1476b
xin <18825188512@163.com>
2019-12-09 22:46:34 +0800

    修改spring boot打包时的bug

---
+++ api-manager/src/main/java/com/yrx/dataSourceManager/apimanager/api/ManagerApi.java
@@ -0,0 +1,13 @@
+package com.yrx.dataSourceManager.apimanager.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by r.x on 2019/12/4.
+ */
+@RestController
+@RequestMapping("/manager")
+public class ManagerApi {
+
+}
---
+++ api-manager/src/main/java/com/yrx/dataSourceManager/apimanager/dto/Response.java
@@ -0,0 +1,27 @@
+package com.yrx.dataSourceManager.apimanager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by r.x on 2019/9/1.
+ */
+@Data
+@Builder
+public class Response<T> {
+    private Integer code;
+    private String msg;
+    private T data;
+
+    private static <T> Response response(Integer code, String msg, T data) {
+        return Response.builder().code(code).msg(msg).data(data).build();
+    }
+
+    public static <T> Response<T> success(T data) {
+        return Response.<T>builder().code(1).msg("成功").data(data).build();
+    }
+
+    public static <T> Response<T> fail(T data) {
+        return Response.<T>builder().code(-1).msg("失败").data(data).build();
+    }
+}
